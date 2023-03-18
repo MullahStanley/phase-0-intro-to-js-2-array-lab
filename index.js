b9f22e6ffc0604fb25ed5589322401857a9e2f6c
MullahStanley <mullah.kimani@student.moringaschool.com>
2023-03-18 08:08:18 +0300

    Assignment complete

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Write your solution here!
+ const cats=["Milo", "Otis", "Garfield"];
+ function destructivelyAppendCat() {
+    return cats.push('Ralph');
+ }
+ const destructivelyPrependCat=function(){
+    return cats.unshift('Bob');
+ }
+ function destructivelyRemoveLastCat(){
+    return cats.pop();
+ }
+ function destructivelyRemoveFirstCat(){
+    return cats.shift();
+ }
+ const appendCat=function(){
+    const catsCopy =[...cats,'Broom'];
+    return catsCopy;
+ }
+ const prependCat=function(){
+    const catsCopy=["Arnold",...cats];
+    return catsCopy
+ }
+ const removeLastCat=function(){
+    const lastCatGone=[...cats.slice(0,2)];
+    return lastCatGone;
+ }
+ function removeFirstCat(){
+    const firstCatGone=[...cats.slice(1,3)];
+    return firstCatGone;
+ }
